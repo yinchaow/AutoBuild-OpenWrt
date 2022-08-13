2ba48fabcecf40951559607e8338c2a6348fca35
yinchaow <r@wyc.me>
2022-08-14 00:07:51 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -22,12 +22,14 @@ git clone https://github.com/esirplayground/luci-app-poweroff.git openwrt/packag
 sed -i '$ised -i '"'"'s/OpenWrt/WYC/g'"'"' /etc/config/system' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #6. 
-sed -i '$ised -i \x27$i\\ \\ \\ \\ \\ \\ \\ \\ option gateway \\x27192.168.7.2\\x27\x27 /etc/config/network' openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i '$ised -i \x27$i\\ \\ \\ \\ \\ \\ \\ \\ option broadcast \\x27192.168.7.255\\x27\x27 /etc/config/network' openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i '$ised -i \x27$i\\ \\ \\ \\ \\ \\ \\ \\ option dns \\x27223.6.6.6\\ 223.5.5.5\\ 119.29.29.29\\ 114.114.114.114\\x27\x27 /etc/config/network' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i '$ised -i \x27$i\\ \\ \\ \\ \\ \\ \\ \\ option gateway \\x27192.168.7.2\\x27\x27 /etc/config/network' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i '$ised -i \x27$i\\ \\ \\ \\ \\ \\ \\ \\ option broadcast \\x27192.168.7.255\\x27\x27 /etc/config/network' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i '$ised -i \x27$i\\ \\ \\ \\ \\ \\ \\ \\ option dns \\x27223.6.6.6\\ 223.5.5.5\\ 119.29.29.29\\ 114.114.114.114\\x27\x27 /etc/config/network' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #7. Disable dhcp
-sed -i '$ised -i \x27s\/option\\ start\\ \\x27100\\x27\/option\\ ignore\\ \\x271\\x27\/g\x27 /etc/config/dhcp' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i '$ised -i \x27s\/option\\ start\\ \\x27100\\x27\/option\\ ignore\\ \\x271\\x27\/g\x27 /etc/config/dhcp' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i '$ised -i \x27\/start\/ignore\/g\x27\ /etc/config/dhcp' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i '$ised -i \x27\/100\/1\/g\x27\ /etc/config/dhcp' openwrt/package/lean/default-settings/files/zzz-default-settings
 sed -i '$ised -i \x27\/limit\/d\x27\ /etc/config/dhcp' openwrt/package/lean/default-settings/files/zzz-default-settings
 sed -i '$ised -i \x27\/leasetime\/d\x27\ /etc/config/dhcp' openwrt/package/lean/default-settings/files/zzz-default-settings
 
