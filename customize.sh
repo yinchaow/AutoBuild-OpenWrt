d94e770afe63aa47e2f776f20787b52b11edb432
wyc <r@wyc.me>
2022-09-04 14:23:33 +0800

    Add change default theme

--- customize.sh
+++ customize.sh
@@ -14,19 +14,15 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 #3. Add luci-app-poweroff
 git clone https://github.com/esirplayground/luci-app-poweroff.git openwrt/package/luci-app-poweroff
 
-#4. Add CloudflareSpeedtest's language
-#echo 'CONFIG_PACKAGE_luci-i18n-cloudflarespeedtest-zh-cn=y' >> openwrt/.config
-#echo 'CONFIG_PACKAGE_luci-i18n-cloudflarespeedtest-zh_Hans=y' >> openwrt/.config
-
-#5. Modify hostname
+#4. Modify hostname
 sed -i '$ised -i '"'"'s/OpenWrt/WYC/g'"'"' /etc/config/system' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-#6. Set gateway, broadcast, dns
+#5. Set gateway, broadcast, dns
 sed -i '$ised -i \x27$i\\ \\ \\ \\ \\ \\ \\ \\ option gateway '"'"'"'"'192.168.7.2'"'"'"'"'\x27 /etc/config/network' openwrt/package/lean/default-settings/files/zzz-default-settings
 sed -i '$ised -i \x27$i\\ \\ \\ \\ \\ \\ \\ \\ option broadcast '"'"'"'"'192.168.7.255'"'"'"'"'\x27 /etc/config/network' openwrt/package/lean/default-settings/files/zzz-default-settings
 sed -i '$ised -i \x27$i\\ \\ \\ \\ \\ \\ \\ \\ option dns '"'"'"'"'223.6.6.6\\ 223.5.5.5\\ 119.29.29.29\\ 114.114.114.114'"'"'"'"'\x27 /etc/config/network' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-#7. Disable dhcp
+#6. Disable dhcp
 #sed -i '$ised -i \x27s\/option\\ start\\ \\x27100\\x27\/option\\ ignore\\ \\x271\\x27\/g\x27 /etc/config/dhcp' openwrt/package/lean/default-settings/files/zzz-default-settings
 #sed -i '$ised -i \x27s\/start\ '"'"'"'"'100'"'"'"'"'\/ignore\ '"'"'"'"'1'"'"'"'"'\/g\x27 /etc/config/dhcp' openwrt/package/lean/default-settings/files/zzz-default-settings
 #sed -i '$ised\ -i\ \x27s\/start\\ '"'"'"'"'100'"'"'"'"'\/ignore\\ '"'"'"'"'1'"'"'"'"'\/g\x27\ \/etc\/config\/dhcp' openwrt/package/lean/default-settings/files/zzz-default-settings
@@ -34,6 +30,10 @@ sed -i '$ised -i '"'"'s/start/ignore/g'"'"' /etc/config/dhcp' openwrt/package/le
 sed -i '$ised -i '"'"'s/100/1/g'"'"' /etc/config/dhcp' openwrt/package/lean/default-settings/files/zzz-default-settings
 sed -i '$ised -i \x27\/limit\/d\x27\ /etc/config/dhcp' openwrt/package/lean/default-settings/files/zzz-default-settings
 sed -i '$ised -i \x27\/leasetime\/d\x27\ /etc/config/dhcp' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#7. Change default theme
+sed -i 's/Bootstrap/Argonne/g' openwrt/feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argonne/g' openwrt/feeds/luci/collections/luci/Makefile
 
 # Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
